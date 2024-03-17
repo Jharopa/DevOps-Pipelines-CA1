647f2eb2adb95008443a9a2f9f0cbd9629ee6c08
Alex Burns <jharopa@gmail.com>
2024-03-17 15:03:41 +0000

    cwe-094 python example

--- des.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import DES
-
-key = b'-8B key-'
-cipher = DES.new(key, DES.MODE_OFB)
-plain_text = b"Hello, World!"
-message = cipher.iv + cipher.encrypt(plain_text)
-
-print(message)
