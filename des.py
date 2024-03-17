5d4df1e66a98a0f6c08b7978df649c62e4e79b29
Alex Burns <jharopa@gmail.com>
2024-03-17 14:57:05 +0000

    DES usage

---
+++ des.py
@@ -0,0 +1,8 @@
+from Crypto.Cipher import DES
+
+key = b'-8B key-'
+cipher = DES.new(key, DES.MODE_OFB)
+plain_text = b"Hello, World!"
+message = cipher.iv + cipher.encrypt(plain_text)
+
+print(message)
