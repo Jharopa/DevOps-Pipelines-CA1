3f7d1fd6e78ff24236bbce80e87690acc62ff8dd
Alex Burns <jharopa@gmail.com>
2024-03-17 15:08:57 +0000

    cwe-094 python example

--- cwe-094.py
+++ cwe-094.py
@@ -1,5 +1,17 @@
-compute = input('\nYour expression? => ')
-if not compute:
-    print ("No input")
-else:
-    print ("Result =", eval(compute))
+urlpatterns = [
+    # Route to code_execution
+    url(r'^code-ex1$', code_execution_bad, name='code-execution-bad'),
+    url(r'^code-ex2$', code_execution_good, name='code-execution-good')
+]
+
+def code_execution(request):
+    if request.method == 'POST':
+        first_name = base64.decodestring(request.POST.get('first_name', ''))
+        #BAD -- Allow user to define code to be run.
+        exec("setname('%s')" % first_name)
+
+def code_execution(request):
+    if request.method == 'POST':
+        first_name = base64.decodestring(request.POST.get('first_name', ''))
+        #GOOD --Call code directly
+        setname(first_name)
