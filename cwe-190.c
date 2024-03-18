176b0e7c8e1f4a38e00d089d2463c38f4b01fc1f
Alex Burns <jharopa@gmail.com>
2024-03-18 17:20:52 +0000

    Multiple langauges

--- cwe-190.c
+++
@@ -1,8 +0,0 @@
-int main(int argc, char** argv) {
-	char buffer[20];
-	fgets(buffer, 20, stdin);
-
-	int num = atoi(buffer);
-	// BAD: may overflow if input is very large
-	int scaled = num + 1000;
-}
