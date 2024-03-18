775c64520b6a0252a15c371b4625f518a8919426
Alex Burns <jharopa@gmail.com>
2024-03-18 17:49:45 +0000

    Include/exclude paths

---
+++ src/tests/unit.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestMethods(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('foo'.upper, 'FOO')
+        
+import sys
+import tarfile
+
+with tarfile.open(sys.argv[1]) as tar:
+    #BAD : This could write any file on the filesystem.
+    for entry in tar:
+        tar.extract(entry, "/tmp/unpack/")
